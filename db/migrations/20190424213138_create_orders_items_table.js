3c361f4031880801440411e6648a135f1fac5462
ferrazf <ferrazf9@gmail.com>
2019-04-24 21:44:59 +0000

    Added database migrations

--- db/migrations/20190424213138_create_orders_items_table.js
+++ db/migrations/20190424213138_create_orders_items_table.js
@@ -1,8 +1,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("orders_items", function(table) {
-    table.increments("id").unsigned().index();
-    table.integer("item_id_FK").unsigned().index();
-    table.integer("order_id_FK").unsigned().index();
+    table
+      .increments("id")
+      .unsigned()
+      .index();
+    table
+      .integer("item_id_FK")
+      .unsigned()
+      .index();
+    table
+      .integer("order_id_FK")
+      .unsigned()
+      .index();
     table.foreign("item_id_FK").references("items.id");
     table.foreign("order_id_FK").references("orders.id");
   });
