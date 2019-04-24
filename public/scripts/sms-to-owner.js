cc87790311c223aebbf6bc59c2359df3bbcd1bcf
RachelTWQ <racheltang15@hotmail.com>
2019-04-24 20:14:03 +0000

    init commit

--- public/scripts/sms-to-owner.js
+++ public/scripts/sms-to-owner.js
@@ -1,3 +1,7 @@
 const sendSMS = require('./send-sms');
 
-sendSMS('+14165206272', "Testing testing"); // take two variables, phone number and text message in string type
+// send cart info to owner
+const ownerNum = process.env.OWNER_NUMBER
+
+let text
+sendSMS(ownerNum, text); // take two variables, phone number and text message in string type
