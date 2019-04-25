2f45c6761809a64da16f1b3e0bfc4ebed04699fa
RachelTWQ <racheltang15@hotmail.com>
2019-04-25 13:18:09 +0000

    add reminder comment for how to run ngrok

--- public/scripts/receive-sms.js
+++ public/scripts/receive-sms.js
@@ -23,3 +23,6 @@ app.post('/', (req, res) => {
 http.createServer(app).listen(1337, () => {
     console.log("Express server listening on port 1337");
 })
+// ngrok http 1337 on a separated terminal and get the http server address
+// save that address to https://www.twilio.com/console/phone-numbers/PN20a898aa406518a1d5df602f824d8a60
+// to update the webhook, http post message incoming
